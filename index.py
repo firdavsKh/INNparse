cc27633e7f1d0443df9e913aaaef553b8941f3e5
NBTstatAMF <mpirov@nbt.tj>
2025-02-24 13:54:54 +0500

    "hello"

---
+++ index.py
@@ -0,0 +1,121 @@
+from fastapi import FastAPI
+import schedule
+import time
+import requests
+import random
+from threading import Thread
+import psycopg2
+from psycopg2.extras import execute_values
+from datetime import datetime
+import traceback
+
+app = FastAPI()
+PORT = 3000
+
+proxies = [
+    'http://183.240.196.55:38080',
+    'http://43.129.201.43:443'
+]
+
+def get_random_proxy():
+    return random.choice(proxies)
+
+def get_random_code():
+    connection = psycopg2.connect(user="docker",password="taxes4omor!",host="localhost",port="5432", database="db_stat_dep")
+    cursor = connection.cursor()
+    sql_query  = 'SELECT * FROM sma_stat_dep.tbl_tax_queue ORDER BY id DESC LIMIT 1'
+    cursor.execute(sql_query)
+    res = cursor.fetchone()
+    print(res)
+    if(res==None):
+        sql_query = "INSERT INTO sma_stat_dep.tbl_tax_queue (inn_code,region_section_code,last_seven_numb,status) VALUES('035000001','03','5000001',true)"
+        cursor.execute(sql_query)
+        connection.commit()
+        res = ['035000001','03','5000001']
+    cursor.close()
+    connection.close()
+    return res
+
+
+def add_one(in_arg):
+    # print(in_arg)
+    return str(int(in_arg) + 1).zfill(len(in_arg))
+
+# print(get_random_code())
+# print(add_one(get_random_code()[1]))
+# print(get_random_code()[1])
+# print(add_one(get_random_code()[2]))
+# print(get_random_code())
+
+
+def job():
+    proxy = get_random_proxy()
+    proxies_dict = {
+        "http": proxy,
+        "https": proxy
+    }
+    try:
+        response = requests.get(f"https://andoz.tj/ForTaxpayer/getInfoByInn?inn={get_random_code()[1]}", proxies=proxies_dict)
+        if response.status_code == 200:
+            connection = psycopg2.connect(user="docker",password="taxes4omor!",host="localhost",port="5432", database="db_stat_dep")
+            cursor = connection.cursor()
+            status = 'false'
+            print(get_random_code()[1])
+            if(response.json().get('userInfo')!=None):
+                print(response.json().get('userInfo'))
+                person = response.json().get('userInfo')[0]
+                # sql_query = f"INSERT INTO sma_stat_dep.tbl_tax_id (inn,upload_tstmp,required_update) VALUES('{get_random_code()[0]}',CURRENT_TIMESTAMP,'false')"
+                # cursor.execute(sql_query)
+
+                sql_query = f"""WITH first_insert AS (
+                        INSERT INTO sma_stat_dep.tbl_tax_id (inn, upload_tstmp, required_update)
+                        VALUES ('{get_random_code()[1]}', CURRENT_TIMESTAMP, 'false')
+                        RETURNING id AS first_table_id
+                    ),
+                    second_insert AS (
+                        INSERT INTO sma_stat_dep.tbl_individuals (tax_id, fst_name, lst_name, full_name, sex, brth_date, citizenship_1, upload_tstmp)
+                        SELECT first_table_id,'{person["Name"]}','{person["Fam"]}','{person["Full_nameK"]}','{person["pol"]}','{person["Date_roj"]}','{person["Citizenship"]}',CURRENT_TIMESTAMP
+                        FROM first_insert
+                    ),
+                    third_insert AS (
+                        INSERT INTO sma_stat_dep.tbl_document (document_id,document_type, issuer, issue_date, upload_tstmp, status)
+                        SELECT '{person["N_Passport"]}','N_Passport','{person["PassportVidanName"]}','{person["Date_vidach"]}',CURRENT_TIMESTAMP,true
+                        FROM first_insert
+                    )
+                    INSERT INTO sma_stat_dep.tbl_addresses (inspection_name,inspection_code,inspection_id,address_text,address_phone,upload_tstmp)
+                    SELECT '{person["inspectionName"]}','{person["inspectionRamz"]}','{person["id_insp"]}','{person["adr_txt"]}','{person["adr_phone"]}',CURRENT_TIMESTAMP
+                    FROM first_insert"""
+                
+                cursor.execute(sql_query)
+                connection.commit()
+                status = 'true'
+            else:
+                status = 'false'
+            print(add_one(get_random_code()[1]))
+            region = get_random_code()[1]
+            if(get_random_code()[2]==9999999):
+                region = add_one(get_random_code()[2])
+            sql_query = f"INSERT INTO sma_stat_dep.tbl_tax_queue (inn_code,region_section_code,last_seven_numb,status) VALUES('{add_one(get_random_code()[1])}','{region}','{add_one(get_random_code()[3])}',{status})"
+            cursor.execute(sql_query)
+            connection.commit()
+            cursor.close()
+            connection.close()
+        else:
+            print(f"Ошибка: {response.status_code}")
+        
+    except Exception as e:
+        print(f"Ошибка: {e}",traceback.print_exc())
+    
+    
+
+def run_scheduler():
+    schedule.every(5).seconds.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    scheduler_thread = Thread(target=run_scheduler)
+    scheduler_thread.start()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
---
+++ initdb.sql
@@ -0,0 +1,77 @@
+CREATE USER docker WITH PASSWORD 'taxes4omor!';
+-- CREATE DATABASE docker;
+
+CREATE DATABASE db_stat_dep WITH ENCODING 'UTF-8' OWNER docker;
+\c db_stat_dep;
+SET ROLE docker;
+CREATE SCHEMA sma_stat_dep;
+
+-- GRANT ALL PRIVILEGES ON DATABASE db_stat_dep TO docker;
+-- GRANT ALL PRIVILEGES ON SCHEMA sma_stat_dep TO docker;
+
+CREATE TABLE sma_stat_dep.tbl_tax_id (
+  id SERIAL PRIMARY KEY,
+  inn VARCHAR(9) UNIQUE,
+  upload_tstmp TIMESTAMP,
+  update_tstmp TIMESTAMP,
+  required_update BOOLEAN
+);
+
+CREATE TABLE sma_stat_dep.tbl_individuals (
+  id SERIAL PRIMARY KEY,
+  tax_id INT REFERENCES sma_stat_dep.tbl_tax_id(id),
+  fst_name VARCHAR(30), 
+  lst_name VARCHAR(30), 
+  full_name VARCHAR(60),
+  sex BOOLEAN,
+  brth_date DATE,
+  citizenship_1 INT,
+  citizenship_2 INT,
+  upload_tstmp TIMESTAMP,
+  update_tstmp TIMESTAMP,
+  required_update BOOLEAN
+);
+
+CREATE TABLE sma_stat_dep.tbl_document (
+  id SERIAL PRIMARY KEY,
+  document_type VARCHAR (100),
+  document_id VARCHAR (100),
+  issuer VARCHAR (100),
+  issue_date DATE,
+  expiration_date DATE,
+  upload_tstmp TIMESTAMP,
+  update_tstmp TIMESTAMP,
+  status BOOLEAN 
+);
+
+CREATE TABLE sma_stat_dep.tbl_addresses (
+  id SERIAL PRIMARY KEY,
+  inspection_name VARCHAR(100),
+  inspection_code INT,
+  inspection_id INT,
+  address_text VARCHAR(255),
+  address_phone VARCHAR(50),
+  address_kpp VARCHAR(50),
+  upload_tstmp TIMESTAMP,
+  update_tstmp TIMESTAMP,
+  status BOOLEAN
+);
+
+CREATE TABLE sma_stat_dep.tbl_sole_enterprise (
+  id SERIAL PRIMARY KEY,
+  tax_id INT REFERENCES sma_stat_dep.tbl_tax_id(id),
+  tax_uid VARCHAR(10) UNIQUE,
+  reg_date DATE,
+  upload_tstmp TIMESTAMP,
+  update_tstmp TIMESTAMP,
+  status BOOLEAN
+);
+
+CREATE TABLE sma_stat_dep.tbl_tax_queue (
+  id SERIAL PRIMARY KEY,
+  inn_code VARCHAR(10),
+  region_section_code INT,
+  last_seven_numb VARCHAR(10),
+  status BOOLEAN
+);
+
